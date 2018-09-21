7cc66fa3b59f27528c9560d83fc70f385343f274
Weisberg Barros <weisbergbsf@gmail.com>
2018-09-21 12:36:54 -0300

    Done project without redux

--- frontend/src/todo/todoList.jsx
+++ frontend/src/todo/todoList.jsx
@@ -10,9 +10,9 @@ export default props => {
                 <td className={todo.done ? 'markedAsDone' : ''}>{todo.description}</td>
                 <td>
                     <IconButton style='success' icon='check' hide={todo.done}
-                        onClick={() => props.markAsDone(todo)}></IconButton>
+                        onClick={() => props.handleMarkAsDone(todo)}></IconButton>
                     <IconButton style='warning' icon='undo' hide={!todo.done} 
-                        onClick={() => props.markAsPending(todo)}></IconButton>
+                        onClick={() => props.handleMarkAsPending(todo)}></IconButton>
                     <IconButton style='danger' icon='trash-o' hide={!todo.done} 
                         onClick={() => props.handleRemove(todo)}></IconButton>
                 </td>
